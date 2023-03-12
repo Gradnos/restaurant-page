1397eb2e9e17ee7e184b54d4f3e7fd25bca162dd
Gradnos <sdolaqidze@gmail.com>
2023-03-12 13:43:39 +0400

    Build dist folder with webpack

--- dist/home.bundle.js
+++ dist/home.bundle.js
@@ -1,26 +1,2 @@
-"use strict";
-(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["home"],{
-
-/***/ "./src/home.js":
-/*!*********************!*\
-  !*** ./src/home.js ***!
-  \*********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "home": () => (/* binding */ home)
-/* harmony export */ });
-function home(){
-    return ' <div class="body"><div class="section-1 section"><div class="img location-img"></div><h2>Best Restaurant On The Island!</h2></div><div class="section-2 section reverse"><h2>Professional Staff, With Expert Skills!</h2><div class="img chef-img"></div></div><div class="section-3 section"><div class="img bus-img"></div><h2>Fast, Easy, Reliable Delivery!</h2></div></div>';
-}
-
-/***/ })
-
-},
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./src/home.js"));
-/******/ }
-]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFPO0FBQ1A7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9ob21lLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBob21lKCl7XG4gICAgcmV0dXJuICcgPGRpdiBjbGFzcz1cImJvZHlcIj48ZGl2IGNsYXNzPVwic2VjdGlvbi0xIHNlY3Rpb25cIj48ZGl2IGNsYXNzPVwiaW1nIGxvY2F0aW9uLWltZ1wiPjwvZGl2PjxoMj5CZXN0IFJlc3RhdXJhbnQgT24gVGhlIElzbGFuZCE8L2gyPjwvZGl2PjxkaXYgY2xhc3M9XCJzZWN0aW9uLTIgc2VjdGlvbiByZXZlcnNlXCI+PGgyPlByb2Zlc3Npb25hbCBTdGFmZiwgV2l0aCBFeHBlcnQgU2tpbGxzITwvaDI+PGRpdiBjbGFzcz1cImltZyBjaGVmLWltZ1wiPjwvZGl2PjwvZGl2PjxkaXYgY2xhc3M9XCJzZWN0aW9uLTMgc2VjdGlvblwiPjxkaXYgY2xhc3M9XCJpbWcgYnVzLWltZ1wiPjwvZGl2PjxoMj5GYXN0LCBFYXN5LCBSZWxpYWJsZSBEZWxpdmVyeSE8L2gyPjwvZGl2PjwvZGl2Pic7XG59Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+"use strict";(self.webpackChunkrestaurant_page=self.webpackChunkrestaurant_page||[]).push([[177],{666:(s,i,e)=>{function a(){return' <div class="body"><div class="section-1 section"><div class="img location-img"></div><h2>Best Restaurant On The Island!</h2></div><div class="section-2 section reverse"><h2>Professional Staff, With Expert Skills!</h2><div class="img chef-img"></div></div><div class="section-3 section"><div class="img bus-img"></div><h2>Fast, Easy, Reliable Delivery!</h2></div></div>'}e.d(i,{L:()=>a})}},s=>{s(s.s=666)}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS5idW5kbGUuanMiLCJtYXBwaW5ncyI6ImdIQUFPLFNBQVNBLElBQ1osTUFBTyxtWEFDWCxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2hvbWUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGZ1bmN0aW9uIGhvbWUoKXtcbiAgICByZXR1cm4gJyA8ZGl2IGNsYXNzPVwiYm9keVwiPjxkaXYgY2xhc3M9XCJzZWN0aW9uLTEgc2VjdGlvblwiPjxkaXYgY2xhc3M9XCJpbWcgbG9jYXRpb24taW1nXCI+PC9kaXY+PGgyPkJlc3QgUmVzdGF1cmFudCBPbiBUaGUgSXNsYW5kITwvaDI+PC9kaXY+PGRpdiBjbGFzcz1cInNlY3Rpb24tMiBzZWN0aW9uIHJldmVyc2VcIj48aDI+UHJvZmVzc2lvbmFsIFN0YWZmLCBXaXRoIEV4cGVydCBTa2lsbHMhPC9oMj48ZGl2IGNsYXNzPVwiaW1nIGNoZWYtaW1nXCI+PC9kaXY+PC9kaXY+PGRpdiBjbGFzcz1cInNlY3Rpb24tMyBzZWN0aW9uXCI+PGRpdiBjbGFzcz1cImltZyBidXMtaW1nXCI+PC9kaXY+PGgyPkZhc3QsIEVhc3ksIFJlbGlhYmxlIERlbGl2ZXJ5ITwvaDI+PC9kaXY+PC9kaXY+Jztcbn0iXSwibmFtZXMiOlsiaG9tZSJdLCJzb3VyY2VSb290IjoiIn0=
